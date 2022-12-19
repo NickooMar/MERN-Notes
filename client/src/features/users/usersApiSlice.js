b04ea7a75cd1064c78c2d836d6b3f3d792081805
Nicolás <nicoo.marsili@gmail.com>
2022-12-19 14:40:43 -0300

    Note List

--- client/src/features/users/usersApiSlice.js
+++ client/src/features/users/usersApiSlice.js
@@ -6,41 +6,47 @@ const usersAdapter = createEntityAdapter({});
 const initialState = usersAdapter.getInitialState();
 
 export const usersApiSlice = apiSlice.injectEndpoints({
-  endpoints: (builder) => ({
-    getUsers: builder.query({
-      query: () => "/users",
+  endpoints: (builder) => ({ 
+    getUsers: builder.query({  // Se crea un hook automaticamente
+      query: () => "/users", // Especificamos el endpoint
       validateStatus: (response, result) => {
         return response.status === 200 && !result.isError;
-      },
-      keepUnusedDataFor: 5,
+      }, // Validamos que no existan errores al llamar a la API
+      keepUnusedDataFor: 5, // Tiempo que se quedara la información en el cache
+
       transformResponse: (responseData) => {
+        // Obtenemos la respuesta de la query
         const loadedUsers = responseData.map((user) => {
+          // Recorremos la información traida del backend y setemos el id del usuario sin el (_) "NORMALIZADOS"
           user.id = user._id;
           return user;
         });
-        return usersAdapter.setAll(initialState, loadedUsers);
+        return usersAdapter.setAll(initialState, loadedUsers); // Seteamos a todos los usuario para que todos tengan el id sin el (_) "NORMALIZADOS" y los almacenamos en el userAdapter
       },
-      providesTags: (result, error, arg) => {
-        if (result?.ids) {
+
+      providesTags: (result, error, arg) => { // Provedes the tags that can be invalidated
+        if (result?.ids) { //Error if a result has not ID
           return [
             { type: "User", id: "LIST" },
             ...result.ids.map((id) => ({ type: "User", id })),
           ];
-        } else return [{ type: "User", id: "LIST" }];
+        } else return [{ type: "User", id: "LIST" }]; // Fails Safe
       },
     }),
   }),
 });
 
-export const { useGetUsersQuey } = usersApiSlice;
+export const { useGetUsersQuery } = usersApiSlice; // useGetUserQuery HOOK
 
-export const selectUsersResult = usersApiSlice.endpoints.getUsers.select();
+// Retorna el objeto resultante de la query
+export const selectUsersResult = usersApiSlice.endpoints.getUsers.select(); // Llama a getUsers definido anteriormente
 
 const selectUsersData = createSelector(
   selectUsersResult,
-  (usersResult) => usersResult.data
+  (usersResult) => usersResult.data //Normaliza el objeto con id y entidades
 );
 
+// Destructuring del selectUserData selector
 export const {
   selectAll: selectAllUsers,
   selectById: selectUserById,
--- server/server.js
+++ server/server.js
@@ -15,6 +15,7 @@ connectDB();
 
 app.use(express.json());
 
+// app.use(cors(corsOptions));
 app.use(cors(corsOptions));
 
 app.use(logger);
